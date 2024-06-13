a9d14fac57b3cd80e9d6016ec42cdea37eaa6ed6
Marlon Andrei <marlon.andrei@webagencia.com.br>
2024-06-13 14:31:52 -0300

    Implementação da função reverse

--- src/main/java/frc/utils/Macro.java
+++ src/main/java/frc/utils/Macro.java
@@ -1,15 +1,16 @@
 package frc.utils;
+
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.io.PrintWriter;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.util.Collections;
+import java.util.Comparator;
+import frc.utils.Dashboard;
 
 public class Macro {
 
   private ArrayList<Double> x = new ArrayList<Double>();
   private ArrayList<Double> y = new ArrayList<Double>();
+
+  private final Dashboard dash = new Dashboard();
 
   public boolean flagRecordMacro = false;
   public boolean flagPlayMacro = false;
@@ -75,6 +76,23 @@ public class Macro {
   public void Add(double x, double y) {
     this.x.add(x);
     this.y.add(y);
+  }
+
+
+  public void reverseXY() {
+    int key = 0;
+
+    ArrayList<Double> localx = new ArrayList<Double>();
+    ArrayList<Double> localy = new ArrayList<Double>();
+    
+    for (int cont=this.x.size()-1;cont>=0;cont--) {
+      localx.add(key,this.x.get(cont) * -1);
+      localy.add(key,this.y.get(cont) * -1);
+      key++;
+    }
+
+    this.x = localx;
+    this.y = localy;
   }
 
   public double getX(int key) {
